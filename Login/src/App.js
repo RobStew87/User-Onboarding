6c8e322c905ba75f2391410f8ff478ac5c847534
Robert Stewart <rstewart414@gmail.com>
2019-11-07 16:42:34 -0500

    mvp and stretch

---
+++ Login/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import OnBoardForm from "./Component/Form";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <OnBoardForm />
+      </div>
+    );
+  }
+}
+
+export default App;
